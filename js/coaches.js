2e53953939aa62e23ee257ede478f99d4f287bce
Talwinder Singh <nijjar.login@gmail.com>
2013-05-31 16:34:46 -0700

    Started click for coach

--- js/coaches.js
+++ js/coaches.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 $(document).ready(function() {
-	
+	$('.contentContainer').hide();	
 	$('#coach li img').hover(function(){
 		var isCoachMoving=false;
 		var currentImageId=$(this).attr('id');
@@ -19,16 +19,35 @@ $(document).ready(function() {
 	},function(){
 		var currentImageId=$(this).attr('id');
 		if(currentImageId<=3){
-				$(this).parent().stop(true,false).animate({left: '-=8'},300,function(){
+				$(this).parent().stop(true,false).animate({left: '-=8'},100,function(){
 					isCoachMoving = false;					
 				});
 			}
 			if(currentImageId>3){
-				$(this).parent().stop(true,false).animate({left: '+=8'},300,function(){
+				$(this).parent().stop(true,false).animate({left: '+=8'},100,function(){
 				isCoachMoving = false;				
 				});
+			}		
+	});
+	
+	$('#coach li img').click(function(){
+		var currentLeft=$(this).parent().offset().left;
+		var currentRight=$(this).parent().offset().right;
+		var currentImageId=$(this).attr('id');
+				
+		$('.contentContainer').hide('clip',400,function(){
+			if(currentImageId!=5){
+				$('.contentContainer').css('left',currentLeft);
+				$('.contentContainer').show('clip',1000);
 			}
+			else{
+				$('.contentContainer').css('right',currentRight);
+				$('.contentContainer').show('clip',1000);
+				
+			}
+		});
 		
+			
 	});
     
 });
