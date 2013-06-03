be64078306b58dee46d1f226451f9e1593c44ad9
Talwinder Singh <nijjar.login@gmail.com>
2013-06-03 10:57:01 -0700

    Sync

--- js/contact.js
+++ js/contact.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+$(document).ready(function(){
+	$(window).load(function(){
+		
+		$('#form1Submit').click(function(){
+			document.form1.submit();	
+		});
+		$('#form1Submit').hover(function(){
+			$(this).css('cursor','pointer');	
+		},function(){
+			$(this).css('cursor','default');			
+		});	
+		
+	});
+});
