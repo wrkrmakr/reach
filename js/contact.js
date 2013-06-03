620aa07627a1cdf3f9bc6342bce463a96fd553db
Talwinder Singh <nijjar.login@gmail.com>
2013-06-03 11:55:16 -0700

    Started on validation of the form

--- js/contact.js
+++ js/contact.js
@@ -3,13 +3,41 @@ $(document).ready(function(){
 	$(window).load(function(){
 		
 		$('#form1Submit').click(function(){
-			document.form1.submit();	
+			
+			
+			//document.form1.submit();
 		});
 		$('#form1Submit').hover(function(){
 			$(this).css('cursor','pointer');	
 		},function(){
 			$(this).css('cursor','default');			
-		});	
-		
+		});
+		$('#name').blur(function(){
+			var name=$("#name").val();
+			if ((name==null || name==""))
+			  {
+				$('#nameResponse').html('*Name can`t be Empty');
+			  }
+			  else if(name.length<2){
+				$('#nameResponse').html('*Atleast two characters are required');				  
+			  }	
+			 else{
+				 $('#nameResponse').html('');				  
+				 
+				 
+				 }			
+		});
+		$('#inputEmail').blur(function(){
+			var name=$("#inputEmail").val();
+			if ((name==null || name==""))
+			  {
+				$('#emailResponse').html('*E-mail can`t be Empty');
+			  }
+			  				
+		});
+		/*$('#name').focus(function(){
+			$('#nameResponse').html('');
+		});*/
+					
 	});
 });
