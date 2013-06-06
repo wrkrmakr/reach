8418c4e87019b4a9931a547e0f762b6fbc0ed0d0
Talwinder Singh <nijjar.login@gmail.com>
2013-06-06 13:29:41 -0700

    sync

--- js/coaches.js
+++ js/coaches.js
@@ -18,8 +18,7 @@ $(document).ready(function() {
 			$(this).parent().stop(true,true).animate({left: '-=1%'},500,function(){
 				isCoachMoving=true;		
 			});
-		}
-		
+		}		
 	},function(){
 		var currentImageId=$(this).attr('id');
 		if(currentImageId<=3){
@@ -33,12 +32,14 @@ $(document).ready(function() {
 				});
 			}		
 	});
+	var coachBio = Array();
+	coachBio[0]="Craig is a Registered Massage Therapist and has been practicing since 1996. She has experience in 					 	treating a range of";
+	coachBio[1]="Jacob firmly believes that a healthy psychology and internal awareness are the essential components of any type  	of training. ";
+	coachBio[2]="Adena firmly believes that a healthy psychology and internal awareness are the essential components of any type  	of training. ";
+	coachBio[3]="Ben believes that achieving goals is dependent on both physical and mental strength.  ";
+	coachBio[4]="Clients who work with Heather receive a program customized to fit their lifestyle and needs.  ";
 	
-	
-	
-	
-	$('.coach img').click(function(){
-		
+	$('.coach img').click(function(){		
 		var currentLeft=$(this).parent().offset().left;
 		var currentImageId=$(this).attr('id');
 		var currentParentDiv = $(this).parent().attr('id');
@@ -46,24 +47,26 @@ $(document).ready(function() {
 		if(currentContainerView != currentParentDiv){
 			if(currentImageId==1){
 				$('.contentContainer').hide('fold',400,function(){
-					$('.contentContainer').css('left',currentLeft+150);
-					
-					
-					
-					
-					$('.contentContainer').show('fold',1000);			
+					$('.contentContainer').animate({left:currentLeft+150},200,function(){
+						$('.contentContainer').show('fold',1000);						
+					});
+					$('.contentBox').html(coachBio[currentImageId-1]);
 				});
 			}
 			if((currentImageId>1)&& (currentImageId<5)){
 				$('.contentContainer').hide('fold',400,function(){
-					$('.contentContainer').css('left',currentLeft);
-					$('.contentContainer').show('fold',1000);			
+					$('.contentContainer').animate({left:currentLeft},200,function(){
+						$('.contentContainer').show('fold',1000);						
+					});
+					$('.contentBox').html(coachBio[currentImageId-1]);
 				});
 			}
 			if(currentImageId==5){
 				$('.contentContainer').hide('fold',400,function(){
-					$('.contentContainer').css('left',currentLeft-70);
-					$('.contentContainer').show('fold',1000);			
+					$('.contentContainer').animate({left:currentLeft-70},200,function(){
+						$('.contentContainer').show('fold',1000);						
+					});
+					$('.contentBox').html(coachBio[currentImageId-1]);
 				});
 			}
 			currentContainerView = currentParentDiv;
