7a641d0b2ec451647f348a4c17cc6d4fe2777beb
Talwinder Singh <nijjar.login@gmail.com>
2013-06-11 15:40:47 -0700

    Sync

--- js/contact.js
+++ js/contact.js
@@ -32,15 +32,15 @@ $(document).ready(function(){
        },
        messages: {
             "name": {
-                required: "<br />&nbsp;&nbsp;Please enter your full name",
+                required: "Please enter your full name",
 				minlength: jQuery.format("At least {0} characters required")
             },
 			"inputEmail": {
-                required: "<br />&nbsp;&nbsp;Please enter your e-mail address",
-				email: "<br />&nbsp;&nbsp;Please enter a valid e-mail address"
+                required: "Please enter your e-mail address",
+				email: "Please enter a valid e-mail address"
             },
 			"mailMessage": {
-                required: "<br /><br />&nbsp;&nbsp;Please send us a note"
+                required: "Please send us a note"
             }
        },
 	    submitHandler: function(form) {
@@ -57,6 +57,56 @@ $(document).ready(function(){
 	    	});
 			return false;
 		}	
-	   	
+	   
+		
 	});
+	
+	/*$('#form1Submit').hover(function(){
+		$(this).css('cursor','pointer');	
+	},function(){
+		$(this).css('cursor','default');			
+	});*/
+
+	/*$('#name').blur(function(){
+		var name=$("#name").val();
+		if ((name==null || name=="")){
+			$('#nameResponse').html('*Name is required');
+		}
+		else if(name.length<2){
+			$('#nameResponse').html('*Atleast two characters are required');				  
+		}	
+		else{
+			$('#nameResponse').html('');				  
+		}			
+	});
+	$('#inputEmail').blur(function(){
+		var email=$("#inputEmail").val();
+		var emailPattern=/^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/; // email pattern
+		if ((email==null || email=="")){
+			$('#emailResponse').html('*E-mail is required');
+		}
+		else if(!(emailPattern.test(email))){
+			$('#emailResponse').html('*E-mail address not valid'); 
+		}
+		else{
+			$('#emailResponse').html('');				
+		}
+		  				
+	});
+	$('#message').blur(function(){
+		var message=$("#message").val();	
+		if ((message==null || message=="")){
+			$('#messageResponse').html('*Message is required');
+		}			
+		else if(message.length<2){
+			$('#messageResponse').html('*Atleast two characters are required');				  
+		}
+		else{
+			$('#messageResponse').html('');				
+		}
+		  				
+	});
+	/*$('#name').focus(function(){
+		$('#nameResponse').html('');
+	});*/		
 });
