f325dde9e8922d9e7352044bb91fd515018ec404
Polos <backend@backend.(none)>
2013-06-13 16:37:19 -0400

    added functionality to make the services and treatments tab toggle if necessary

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
 $(document).ready(function(){
 	$(".contentBox").mCustomScrollbar({theme:"dark"});
-	$(".bgimage, #logo, #homeCarousel, #coach").click(function(){
-		$(".nav-collapse").collapse('hide');
-	});
 });
--- js/treatment.js
+++ js/treatment.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
 			changeContent();
 		}
 		$(".nav-collapse").collapse('hide');
+		$(".dropdown-toggle").dropdown('toggle');
 	});
 
 	$("#leftscroll").click(function(){
